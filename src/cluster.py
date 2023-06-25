13c423868addaddb1eae6474367147f4b64f68d6
Hamed Karbasi <hamed.karbasi.93@gmail.com>
2023-06-25 11:17:55 +0330

    add different storage per node

--- src/cluster.py
+++ src/cluster.py
@@ -2,19 +2,24 @@ from itertools import cycle
 import statistics
 
 class Node:
-    def __init__(self) -> None:
+    def __init__(self, size) -> None:
         self.shard_list = []
-
+        self.size = size
 
 class Cluster:
-    def __init__(self, n_nodes, n_replicas, data_size, n_shards, storage_per_node):
+    def __init__(self, n_nodes, n_replicas, data_size, n_shards, nodes_storage_list):
         self.n_shards = n_shards
         self.n_replicas = n_replicas
         # data_size is the original size without replication
         self.data_size = data_size
-        self.storage_per_node = storage_per_node
         self.n_nodes = n_nodes
-        self.nodes = [Node() for _ in range(n_nodes)]
+        if isinstance(nodes_storage_list, int):
+            storage_per_node = nodes_storage_list
+            nodes_storage_list = [storage_per_node for _ in range(n_nodes)]
+        else:
+            assert len(nodes_storage_list) == n_nodes
+        self.cluster_storage_size = sum(nodes_storage_list)
+        self.nodes = [Node(nodes_storage_list[i]) for i in range(n_nodes)]
 
     def shard_allocator(self):
         # Round-roubin method
@@ -25,10 +30,11 @@ class Cluster:
                 candidate = self.nodes[pool.__next__()]
                 candidate.shard_list.append(i)
 
+
     def cluster_score(self):
         # lower is better
         total_data_size = self.data_size * self.n_replicas
-        if total_data_size > (self.n_nodes * self.storage_per_node):
+        if total_data_size > (self.cluster_storage_size):
             return -1
         inhomogeneity = statistics.stdev(
             [len(node.shard_list) for node in self.nodes]
--- src/main.py
+++ src/main.py
@@ -1,13 +1,27 @@
 import argparse
 from cluster import Cluster
 
+def int_or_list(arg):
+    # Check if the input is a single integer
+    try:
+        return int(arg)
+    except ValueError:
+        pass
+    
+    # Check if the input is a comma-separated list of integers
+    try:
+        return [int(x) for x in arg.split(',')]
+    except ValueError:
+        raise argparse.ArgumentTypeError("Invalid input: Expected an integer or a \
+                                         comma-separated list of integers.")
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Cluster Allocation")
     parser.add_argument("--n_nodes", type=int, default=4, help="Number of nodes")
     parser.add_argument("--n_shards", type=int, default=2, help="Number of shards")
     parser.add_argument("--n_replicas", type=int, default=2, help="Number of replicas")
     parser.add_argument("--data_size", type=int, default=2, help="Data size in TB")
-    parser.add_argument("--storage_per_node", type=int, default=1, 
+    parser.add_argument("--nodes_storage_list", type=int_or_list, default=1, 
                         help="Storage per node in TB")
     args = parser.parse_args()
 
@@ -15,14 +29,14 @@ if __name__ == "__main__":
     n_shards = args.n_shards
     n_replicas = args.n_replicas
     data_size = args.data_size
-    storage_per_node = args.storage_per_node
+    nodes_storage_list = args.nodes_storage_list
 
-    cluster = Cluster(n_nodes, n_replicas, data_size, n_shards, storage_per_node)
+    cluster = Cluster(n_nodes, n_replicas, data_size, n_shards, nodes_storage_list)
     cluster.shard_allocator()
     print(
         "Number of nodes: {} \nNumber of shards: {} \nNumber of replicas: {} \n \
-            Data size: {} TB\nStorage per node: {} TB\n".format(
-            n_nodes, n_shards, n_replicas, data_size, storage_per_node
+            Data size: {} TB\nNodes storage list: {} TB\n".format(
+            n_nodes, n_shards, n_replicas, data_size, nodes_storage_list
         )
     )
 
